dbf7b5e990586195838e7600e4f11f622edccef5
Miklos Bukva <39128681+bukvamikk@users.noreply.github.com>
2020-05-13 19:19:57 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="ToDo App">
+      <div>
+        <label>
+          <input type="checkbox" />
+          Something to do
+        </label>
+        <br />
+        <label>
+          <input type="checkbox" />
+          Something to do
+        </label>
+        <br />
+        <label>
+          <input type="checkbox" />
+          Something to do
+        </label>
+        <br />
+        <label>
+          <input type="checkbox" />
+          Something to do
+        </label>
+        <br />
+        <label>
+          <input type="checkbox" />
+          Something to do
+        </label>
+      </div>
+    </div>
+  );
+}
