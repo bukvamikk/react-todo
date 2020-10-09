53783bdfc9bccd2731131b7b5165bd799b4256a0
Miklos Bukva <39128681+bukvamikk@users.noreply.github.com>
2020-10-09 19:10:15 +0200

    make it into class class and state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import "./styles.css";
 import TodoItem from "./TodoItem";
 import TodosData from "./TodosData";
 
-export default function App() {
-  const todoItem = TodosData.map((item) => {
-    return <TodoItem key={item.id} item={item} />;
-  });
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: TodosData
+    };
+  }
 
-  return (
-    <div className="ToDo App">
-      <div className="todo-list">{todoItem}</div>
-    </div>
-  );
+  render() {
+    const todoItem = this.state.todos.map((item) => {
+      return <TodoItem key={item.id} item={item} />;
+    });
+
+    return (
+      <div className="ToDo App">
+        <div className="todo-list">{todoItem}</div>
+      </div>
+    );
+  }
 }
+
+export default App;
